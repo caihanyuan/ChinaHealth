b30a54cd2808ecfd37838c3db38f823873e1194f
caihanyuan <574583546@qq.com>
2015-08-31 11:00:18 +0800

    [国学养生]定义Homepage Fragment基类。抽象拖动页面方法。 变更类别:需求管理 适用机型:all 验证建议:none 关联变更项: 无

--- chinahelthmain/src/main/java/com/chinahelth/ui/homepages/HomepageBaseFragment.java
+++ chinahelthmain/src/main/java/com/chinahelth/ui/homepages/HomepageBaseFragment.java
@@ -2,65 +2,71 @@ package com.chinahelth.ui.homepages;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.ListView;
-import android.widget.ScrollView;
 
-import com.chinahelth.ui.homepages.base.RemoteSyncInterface;
+import com.chinahelth.R;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.listener.PauseOnScrollListener;
 
 /**
  * Created by caihanyuan on 15-8-22.
  */
-public class HomepageBaseFragment extends Fragment implements PullToRefreshBase.OnRefreshListener<ScrollView>, RemoteSyncInterface {
+public class HomepageBaseFragment extends Fragment implements PullToRefreshBase.OnRefreshListener {
+
+    private AbsListView.OnScrollListener mOnScrollListener;
 
     protected View mViewRoot;
 
     protected ListView mListView;
 
-    protected ScrollView mScrollView;
+    protected PullToRefreshListView mRefreshListView;
 
     protected HomePageContentAdapter mAdapter;
+
+    protected PauseOnScrollListener mPauseOnScrollListener;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mViewRoot = inflater.inflate(R.layout.homepages_fragment_layout, container, false);
+        mRefreshListView = (PullToRefreshListView) mViewRoot.findViewById(R.id.homepage_refreshview);
+        mListView = mRefreshListView.getRefreshableView();
+        mRefreshListView.setOnRefreshListener(this);
+        return mViewRoot;
+    }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         initView();
         initData();
-        setScrollListener();
+        setPauseScrollListener();
     }
 
     protected void initView() {
-
     }
 
     protected void initData() {
 
     }
 
-    protected void setScrollListener() {
-        if (mScrollView != null) {
+    public void setOnScrollListener(AbsListView.OnScrollListener onScrollListener) {
+        mOnScrollListener = onScrollListener;
+    }
+
+    @Override
+    public void onRefresh(PullToRefreshBase refreshView) {
+
+    }
+
+    private void setPauseScrollListener() {
+        if (mPauseOnScrollListener == null) {
+            mPauseOnScrollListener = new PauseOnScrollListener(ImageLoader.getInstance(), true, true, mOnScrollListener);
         }
-        if (mListView != null) {
-        }
-    }
-
-    @Override
-    public void onRefresh(PullToRefreshBase<ScrollView> refreshView) {
-
-    }
-
-    @Override
-    public void setRemoteSyncListner(RemoteSyncListenr remoteSyncListner) {
-
-    }
-
-    @Override
-    public void loadDataInDatabase() {
-
-    }
-
-    @Override
-    public void syncDataFromRemote() {
-
+        mRefreshListView.setOnScrollListener(mPauseOnScrollListener);
     }
 }
--- chinahelthmain/src/main/java/com/chinahelth/ui/homepages/HomepageBuddhismFragment.java
+++ chinahelthmain/src/main/java/com/chinahelth/ui/homepages/HomepageBuddhismFragment.java
@@ -1,38 +1,8 @@
 package com.chinahelth.ui.homepages;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 /**
  * Created by caihanyuan on 15-7-14.
  */
-public class HomepageBuddhismFragment extends Fragment {
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-    }
+public class HomepageBuddhismFragment extends HomepageBaseFragment {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-    }
 }
